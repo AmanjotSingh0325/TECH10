d58f0afef7db513d2fa393134fb65fbe92471305
AmanjotSingh0325 <amanjotsingh0325@gmail.com>
2025-11-04 15:29:42 +0530

    logo path changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return <div className="flex items-center justify-between shadow-[#cd5a08] shadow-md py-4 px-8">
-        <div className="w-52"><img src="public\logo-removebg-preview.png" alt="logo" /></div>
+        <div className="w-52"><img src="./logo-removebg-preview.png" alt="logo" /></div>
         <ul className="flex items-center justify-center gap-7 text-black font-bold text-xl">
             <li className="hover:text-white hover:bg-[#cd5a08] p-2 rounded-3xl">Home</li>
             <li className="hover:text-white hover:bg-[#cd5a08] p-2 rounded-3xl">About</li>
